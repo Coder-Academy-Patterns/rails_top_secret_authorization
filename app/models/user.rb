8ab6200047abed432df15b2de023e61693e78353
Patrick Smith <patrick@burntcaramel.com>
2017-10-26 11:53:23 +1100

    Work out company from user’s email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  # e.g. with email 'sue@spacex.com', will return 'spacex.com'
+  def domain
+    self.email.split('@').last
+  end
+
+  def company
+    Company.find_by(domain: self.domain)
+  end
 end
