91ad057c1a1f93b1192a3bd1714703c1e529577a
Patrick Smith <patrick@burntcaramel.com>
2017-10-26 12:46:21 +1100

    Add authorization for editing documents

--- app/policies/document_policy.rb
+++ app/policies/document_policy.rb
@@ -1,5 +1,15 @@
 class DocumentPolicy < ApplicationPolicy
   def show?
+    # If current user belongs to the same company as the document
+    if user.company == record.company
+      true
+    else
+      false
+    end
+  end
+
+  def update?
+    # If current user belongs to the same company as the document
     if user.company == record.company
       true
     else
