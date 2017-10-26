9e64f4c14c3f615c644db8b64693718c5ae7cb7a
Patrick Smith <patrick@burntcaramel.com>
2017-10-26 11:36:22 +1100

    Add documents #index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'documents#index'
+  resources :documents, only: [:show]
+  get '/documents' => redirect('/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
