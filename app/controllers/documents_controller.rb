ce65faf7003d9cea37089c7b3aeca2aa4faee273
Patrick Smith <patrick@burntcaramel.com>
2017-10-26 12:50:44 +1100

    Refactor documents set up with before_action method

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,6 @@
 class DocumentsController < ApplicationController
+  before_action :set_document, only: [:show, :edit, :update]
+
   def index
     if signed_in?
       company = current_user.company
@@ -7,18 +9,12 @@ class DocumentsController < ApplicationController
   end
 
   def show
-    @document = Document.find(params[:id])
-    authorize @document
   end
 
   def edit
-    @document = Document.find(params[:id])
-    authorize @document
   end
 
   def update
-    @document = Document.find(params[:id])
-    authorize @document
     if @document.update(document_params)
       # Success, got to #show page
       redirect_to @document
@@ -32,5 +28,10 @@ class DocumentsController < ApplicationController
     # What attributes we allow from forms
     def document_params
       params.require(:document).permit(:name, :content)
+    end
+
+    def set_document
+      @document = Document.find(params[:id])
+      authorize @document
     end
 end
